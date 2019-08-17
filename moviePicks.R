b49c39377dd2910ca3538ba8464e3ce096b0b124
Tom Linnell <telemeter@protonmail.com>
2019-08-17 12:53:25 -0400

    Update with latest R/Rmd files

--- moviePicks.R
+++ moviePicks.R
@@ -56,7 +56,9 @@ validation <- temp %>%
 removed <- anti_join(temp, validation)
 edx <- rbind(edx, removed)
 
-rm(dl, ratings, movies, test_index, temp, movielens, removed)
+# Clean up data structures no longer needed
+
+rm(dl, ratings, movies, test_index, temp, removed)
 
 ###################################################################
 #
@@ -86,7 +88,7 @@ length(unique(edx$userId))
 # Q5 - How many movie ratings are in each of the following genres in the edx dataset?
 # Here we want to group summaries by genre:
 
-#create subset for testing code:
+#create test subset (can be handy when checking code):
 t_edx <- edx[1:100, ]
 test_rating <- separate_rows(t_edx, genres)
 
@@ -117,78 +119,81 @@ edx %>% ggplot(aes(rating)) + geom_histogram()
 # 
 ###################################################################
 #
-# Section 1. Introduction 
-# 
-# The goal of this project is to create a recommendation system that will predict
-# with high accuracy the rating that a customer would give to any particular movie
-# that they would watch. This would be useful to organizations that provide movies
-# as a service to help guide customers to select movies that they would like to see,
-# and to avoid selecting movies that they would not. 
-#
-# The information that we have available to us is a large database of movie ratings 
-# ("movielens") by many users - in this case, ratings of about 10,000 movies rated by
-# almost 70,000 viewers. However, this database is "sparse" in that we don't have ratings
-# for all movies by all viewers. The challenge is to be able to use the information 
-# available to design a system that would be able to provide a good estimate of a 
-# particular users rating of a movie that they haven't rated yet.
-#
-# It turns out that this is a fairly complicated problem to solve, and involves much
-# more that just providing the average review rating from all previous viewers of the 
-# movie. There are many factors to consider - some people like different types of movies
-# more than others, some movies are more liked than others by the general population, 
-# the user may be swayed by the actors featured in a movie, or the fact that all their 
-# friends are talking about a movie, and so on.
-#
-# Because of all these factors, we have chosen to use a machine learning algorithm approach
-# to solving this puzzle. This will allow us to rapidy modify our modeling approaches
-# utilizing a wide range of available techniques, and to iteratively add components to our 
-# model and to be able to test the effects of these on the overall accuracy.
-# The goal of this project is to reduce the estimate error to a number below 0.9 RMSE,
-# with increasing project scores as this metric approaches 0.8649.
-#
-# To begin with , we take the movielens 10M database provided and split this into a 
-# test set (90%) and a validation set (10%). Our methodology will be developed on the 
-# test set, and measured on the validation set, using the RMSE metric to judge and 
-# compare the results as noted above.
-#
-###################################################################
-#
 # Methods and analysis
 # 
-# We start by examing our dataset for data quality, first with summary, to see 
-# if there are any NA's:
-summary(edx)
-# we will also check the range on the rating to make sure there are no outliers:
-min(edx$rating)
-max(edx$rating)
-# So the data looks OK. We can now proceed to analyzing these data.
+
+# We start by examing our dataset for data quality, checking to see 
+# if there are any NA's, ranges and outliers, etc.:
+summary(movielens)
+glimpse(movielens)
+# So the data looks OK. We can now proceed to analyzing these data to come up with 
+# an accurate prediction methodology. From the reference literature and coursework 
+# that we recently completed, we know that our model will require at least a 
+# computation of the following:
+#
+# A. A baseline rating (e.g., the mean of all ratings)
+# B. A movie-specific effect (e.g., how this movie is rated vs. all of the other movies)
+# C. A user-specific effect (similar to the above, how this user compares to the others)
+#
+# In addition, it may be helpful to regularize the ratings data to reduce the 
+# noisiness of movie ratings with low sampling counts. We will use this technique and
+# compare the results to the unmodified dataset.
+
 
 # computing a baseline mean over all user-movie ratings:
 (mu_hat <- mean(edx$rating))
 
-# Now we compute our first RMSE using just the baseline mean:
+# let's also build a function to compute the RMSE of our model against a
+# test data set:
 RMSE <- function(true_ratings, predicted_ratings){
   sqrt(mean((true_ratings - predicted_ratings)^2))
 }
 
-(naive_rmse <- RMSE(validation$rating, mu_hat))
+# Now we compute our first RMSE using just the baseline mean:
+(baseline_rmse <- RMSE(validation$rating, mu_hat))
+
+# let's put the results of our calculations into a table, so that we can 
+# compare them in the final section:
+rmse_results <- data_frame(method = "Baseline (mean)", RMSE = baseline_rmse)
 
 # This isn't very accurate, so we will add both a movie effect and a user effect
 # to this calculation: 
 
+# We begin with the movie effect.
 # We compute the difference of each movie mean rating from our mu_hat, above:
 movie_avgs <- edx %>% 
   group_by(movieId) %>% 
   summarize(b_i = mean(rating - mu_hat))
 
-predicted_ratings_1 <- mu_hat + validation %>% 
+movie_avgs %>% qplot(b_i, geom ="histogram", bins = 10, data = ., color = I("black"))
+
+# Now we create a set of predictions for the validation set by using the baseline
+# and movie effect calculations above for each observation:
+predicted_ratings <- mu_hat + validation %>% 
   left_join(movie_avgs, by='movieId') %>%
   pull(b_i)
 
-(model_1_rmse <- RMSE(predicted_ratings_1, validation$rating))
+# now we see how much of an improvement this yields:
+(movie_rmse <- RMSE(predicted_ratings, validation$rating))
+
+# and we save that into our results table:
+rmse_results <- bind_rows(rmse_results,
+                          data_frame(method="Movie Effect Model",  
+                                     RMSE = movie_rmse))
 
 #Now we add user effect. Similarly, this computes the difference in the mean 
 # of each user, once the baseline and movie effects have been removed:
+
+#Let's look at the distribution of user ratings:
+
+edx %>% 
+  group_by(userId) %>% 
+  summarize(b_u = mean(rating)) %>% 
+  filter(n()>=100) %>%
+  ggplot(aes(b_u)) + 
+  geom_histogram(bins = 30, color = "black")
+
+
 user_avgs <- edx %>% 
   left_join(movie_avgs, by='movieId') %>%
   group_by(userId) %>%
@@ -200,35 +205,35 @@ predicted_ratings <- validation %>%
   mutate(pred = mu_hat + b_i + b_u) %>%
   pull(pred)
 
-(model_2_rmse <- RMSE(predicted_ratings, validation$rating))
+(user_rmse <- RMSE(predicted_ratings, validation$rating))
+
+rmse_results <- bind_rows(rmse_results,
+                          data_frame(method="Movie + User Effects Model",  
+                                     RMSE = user_rmse))
 
 # Since we see that there is such a significant user effect, we want to examine this
 # a bit further to see what is going on here. We now plot the histogram of users 
 # who have rated at least 100 movies:
 
-edx %>% 
-  group_by(userId) %>% 
-  summarize(b_u = mean(rating)) %>% 
-  filter(n()>=100) %>%
-  ggplot(aes(b_u)) + 
-  geom_histogram(bins = 30, color = "black")
-
 movie_avgs %>% qplot(b_i, geom ="histogram", bins = 10, data = ., color = I("black"))
 
 user_avgs %>% qplot(b_u, geom ="histogram", bins = 10, data = ., color = I("black"))
 
-# This plot shows that the user effect is fairly tightly distributed around a 
-# mean of about 3.6, so this verifies that it is a significant effect. 
+
+
 
 # Part II. Can we do better? Now that we have accounted for the baseline, movie,
 # and user effects, our RMSE is at a respectable 0.865. However, we have not
-# yet explored 
+# yet explored whether removing the noise from ratings of movies with low sample
+# counts will improve our score, so we will take another look at the data.
 
+
+# by looking at the movies with the largest errors, we see that
 edx %>% 
   left_join(movie_avgs, by='movieId') %>%
   mutate(residual = rating - (mu_hat + b_i)) %>%
   arrange(desc(abs(residual))) %>% 
-  select(title,  residual) %>% slice(1:10) 
+  select(title,  residual) %>% distinct() %>% slice(1:25) 
 
 movie_titles <- movielens %>% 
   select(movieId, title) %>%
@@ -246,26 +251,26 @@ movie_avgs %>% left_join(movie_titles, by="movieId") %>%
   select(title, b_i) %>% 
   slice(1:10) 
 
-# We now want to regualrize the data. First we need to select our lambda parameter:
+# We now look at how often these movies were rated:
+edx %>% count(movieId) %>% 
+  left_join(movie_avgs) %>%
+  left_join(movie_titles, by="movieId") %>%
+  arrange(desc(b_i)) %>% 
+  select(title, b_i, n) %>% 
+  slice(1:10) 
+
+edx %>% count(movieId) %>% 
+  left_join(movie_avgs) %>%
+  left_join(movie_titles, by="movieId") %>%
+  arrange(b_i) %>% 
+  select(title, b_i, n) %>% 
+  slice(1:10)
+
+
+# We now want to test the effect of regularizing the data. We will compute this
+# for a range of penalty parameters (lambda):
 lambdas <- seq(0, 10, 0.25)
 
-mu <- mean(edx$rating)
-just_the_sum <- edx %>% 
-  group_by(movieId) %>% 
-  summarize(s = sum(rating - mu), n_i = n())
-
-rmses <- sapply(lambdas, function(l){
-  predicted_ratings_l <- validation %>% 
-    left_join(just_the_sum, by='movieId') %>% 
-    mutate(b_i_l = s/(n_i+l)) %>%
-    mutate(pred = mu + b_i_l) %>%
-    pull(pred)
-  return(RMSE(predicted_ratings_l, validation$rating))
-})
-qplot(lambdas, rmses)  
-lambdas[which.min(rmses)]
-
-# The above uses the test set, so let's recalculate using the training data only:
 rmses <- sapply(lambdas, function(l){
   
   mu <- mean(edx$rating)
@@ -293,12 +298,10 @@ qplot(lambdas, rmses)
 
 (lambda <- lambdas[which.min(rmses)])
 
-(model_4_rmse = min(rmses))
-
 # Now we will recompute our factors using the regularized data:
 movie_reg_avgs <- edx %>% 
   group_by(movieId) %>% 
-  summarize(b_i = sum(rating - mu)/(n()+lambda), n_i = n()) 
+  summarize(b_i = sum(rating - mu_hat)/(n()+lambda), n_i = n()) 
 
 # To see the effect, we plot the original movie data vs. our new averages:
 data_frame(original = movie_avgs$b_i, 
@@ -309,3 +312,73 @@ data_frame(original = movie_avgs$b_i,
 
 #Note that the change is especially strong in the lower LH quadrant
 
+edx %>% 
+  left_join(movie_reg_avgs, by='movieId') %>%
+  mutate(residual = rating - (mu_hat + b_i)) %>%
+  arrange(desc(abs(residual))) %>% 
+  select(title,  residual) %>% distinct() %>% slice(1:25) 
+
+# finally, we update our table with the best rmse from the regularized data:
+rmse_results <- bind_rows(rmse_results,
+                          data_frame(method="Regularized Movie + User Effect Model",  
+                                     RMSE = min(rmses)))
+rmse_results %>% knitr::kable()
+
+# Discussion of results.
+
+# Sample some predictions:
+
+b_i_l <- edx %>% 
+  group_by(movieId) %>%
+  summarize(b_i_l = sum(rating - mu_hat)/(n()+lambda))
+
+b_u_l <- edx %>% 
+  left_join(b_i_l, by="movieId") %>%
+  group_by(userId) %>%
+  summarize(b_u_l = sum(rating - b_i_l - mu_hat)/(n()+lambda))
+
+predicted_ratings_l <- 
+  validation %>% 
+  left_join(b_i_l, by = "movieId") %>%
+  left_join(b_u_l, by = "userId") %>%
+  mutate(pred = mu_hat + b_i_l + b_u_l)
+
+# Now lets take a look at some of our predictions:
+
+set.seed(1, sample.kind="Rounding")
+
+index <- sample(1:nrow(predicted_ratings_l), 50)
+
+movie_sample <- predicted_ratings_l[index, ] %>% mutate(error = rating - pred, mu = mu_hat)
+
+movie_plot <- gather(movie_sample, key = effect, value = value, b_i_l:mu, -pred)
+# We want to create a stacked bar chart showing the rating vs. predicted + error
+
+# Create break points and labels for axis ticks
+brks <- movie_sample$movieId[seq(1, length(movie_sample$movieId), 10)]
+lbls <- movie_sample$movieId[seq(1, length(movie_sample$movieId), 10)]
+
+# Plot
+ggplot(movie_sample, aes(userId, error)) + 
+  geom_area() + 
+  scale_x_continuous(breaks=waiver(), labels=waiver()) + 
+  theme(axis.text.x = element_text(angle=90)) + 
+  labs(title="Area Chart", 
+       subtitle = "Prediction Error for Movie Recommendations", 
+       y="Prediction error", 
+       caption="Source: movielens 10M")
+
+movie_sample %>% ggplot(aes(x = title, y = error)) + geom_bar( stat = "identity")
+
+movie_sample %>% ggplot() + geom_bar(aes(title, rating)) + geom_dotplot(aes(pred))
+
+# Then we want to create a stacked bar chart showing the components of the 
+# prediction - mu_hat, b_i, b_u and error.
+
+movie_plot %>% ggplot() + geom_bar(aes(y = rating, x = title, fill = effect), stat = "identity")
+
+# Finally:
+
+summary(predicted_ratings_l)
+                                     
+                                     
